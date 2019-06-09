9d89ce76a55f2b2cee6ca8f33c66f8339ea68a72
skugele <seankugele@gmail.com>
2019-06-09 12:10:23 -0500

    Add markdown to matrix_plot notebook. Clean-up docstrings and correct plot title issue.

--- src/linear_algebra/matrix_plot.py
+++ src/linear_algebra/matrix_plot.py
@@ -1,10 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.cm as cm
 from matplotlib.colors import Normalize
 
 
-def plot_colspace_2d(matrix):
+def plot_column_vectors_2d(matrix):
+    """
+    Plots column vectors from the supplied matrix in the 2D plane.  The matrix must have shape (2,X), where X >= 1.
+    :param matrix: a (2,X) matrix; x >= 1
+    :return: None.  Displays 2D plot.
+    """
     if matrix.shape[0] != 2:
         raise ValueError("Matrix must have 2d column space")
 
@@ -28,7 +32,17 @@ def plot_colspace_2d(matrix):
               scale_units='xy')
 
 
-def plot_colspace_transform_2d(matrix, transform, showgrid=True):
+def plot_column_vectors_with_transform_2d(matrix, transform):
+    """
+    Displays a side-by-side plot of the column vectors in the supplied matrix and the column vectors in the transformed matrix matrix * transform.
+
+    The matrix must have shape (2,X), where X >= 1.
+    The transform must have shape (2,2)
+
+    :param matrix: a (2,X) matrix; x >= 1
+    :param transform: a (2,2) transformation matrix.
+    :return: None. Displays 2 2D subplots.
+    """
     if matrix.shape[0] != 2:
         raise ValueError("Matrix must have 2d column space")
 
@@ -38,7 +52,6 @@ def plot_colspace_transform_2d(matrix, transform, showgrid=True):
     origin = np.zeros_like(matrix)
 
     fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 6))
-    plt.grid(showgrid)
 
     colors = np.linalg.norm(matrix, axis=0)
     colormap = plt.get_cmap('jet')
@@ -52,7 +65,7 @@ def plot_colspace_transform_2d(matrix, transform, showgrid=True):
     ymin, ymax = min(np.min(matrix[1]), 0), max(np.max(matrix[1]), 0)
 
     ax1.axis(list(map(int, [xmin - 1, xmax + 1, ymin - 1, ymax + 1])))
-    ax1.set_title("Before")
+    ax1.set_title("$A$")
     ax1.grid(True)
     ax1.quiver(origin[0], origin[1], matrix[0], matrix[1], color=colormap(norm(colors)), scale=1, angles='xy',
                scale_units='xy')
@@ -61,7 +74,7 @@ def plot_colspace_transform_2d(matrix, transform, showgrid=True):
     ymin, ymax = min(np.min(trans_matrix[1]), 0), max(np.max(trans_matrix[1]), 0)
 
     ax2.axis(list(map(int, [xmin - 1, xmax + 1, ymin - 1, ymax + 1])))
-    ax2.set_title("After")
+    ax2.set_title("$TA$")
     ax2.grid(True)
     ax2.quiver(origin[0], origin[1], trans_matrix[0], trans_matrix[1], color=colormap(norm(colors)), scale=1,
                angles='xy', scale_units='xy')
